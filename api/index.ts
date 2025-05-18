a39839e9e597ea82409e0549e0521aa750e77310
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-05-18 16:51:49 +0700

    try hono node server

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,4 @@
-import { handle } from "hono/vercel";
+import { handle } from "@hono/node-server/vercel";
 import app from "../src/app";
 
-const handler = handle(app);
-
-export const GET = handler;
-export const POST = handler;
-export const PATCH = handler;
-export const PUT = handler;
-export const OPTIONS = handler;
+export default handle(app);
